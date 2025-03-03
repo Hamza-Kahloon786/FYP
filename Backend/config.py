832dd79d9845753731e72e3f969a8cadf6cbc032
Hamza-Kahloon786 <hamzaakahloon903@gmail.com>
2025-03-03 16:03:27 +0500

    Folder Structure

---
+++ Backend/config.py
@@ -0,0 +1,3 @@
+# confi.py
+# MONGO_URI = 'mongodb+srv://tahanaveedbutt12345:tahanaveedbutt12345@cluster0.rzj5r.mongodb.net/db1?retryWrites=true&w=majority'
+MONGO_URI = 'mongodb+srv://hamzaakahloon903:hamzaakahloon903@cluster0.b0ehw.mongodb.net/FYP?retryWrites=true&w=majority&appName=Cluster0'
